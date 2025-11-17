0993b7d0e6dd36529accde6277d7e9f314076041
Viczenith <viczenithgodwin@gmail.com>
2025-11-18 04:42:35 +0700

    Update deployment configuration11

--- estateProject/estateProject/wsgi.py
+++ estateProject/estateProject/wsgi.py
@@ -18,7 +18,7 @@ for path in [str(BASE_DIR), str(PROJECT_ROOT)]:
         sys.path.insert(0, path)
 
 # Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estateProject.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estateProject.settings')
 
 # Initialize Django application
 from django.core.wsgi import get_wsgi_application
